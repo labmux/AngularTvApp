0a460e6a88affe108aca9c4c06e2071c975bf91a
Eliran Assaraf <eliran.a.bg@gmail.com>
2019-03-26 12:08:06 -0400

    toolbar and table views added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,40 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule} from "@angular/forms";
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule} from "@angular/material";
+import {MatButtonModule} from '@angular/material/button';
+    // tables
+import {MatTableModule} from '@angular/material/table';
+import { BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
+// Views
+import { ToolbarComponent} from './views/toolbar/toolbar.component';
+import { DisplayListComponent } from './views/display-list/display-list.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    DisplayListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+      MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
